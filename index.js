9db332e14f4bc774376095e80598df0a6a61896b
MarcoOlivette <marco.vieira.olivette@gmail.com>
2020-09-15 15:55:15 -0300

    create something in database

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const app = express();
 
 mongoose.connect(
-    "mongodb://localhost:27017/test",
-    { useNewUrlParser: true}
+    "mongodb://localhost:27017/test", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
 );
 
+requireDir('./src/models');
 
-app.get("/", (req,res) => {
-    res.send('Ola mundo')
-})
+const Product = mongoose.model('Product');
+
+app.get("/", (req, res) => {
+    Product.create({
+        title: "title",
+        description: "description",
+        url:"www.google.com"
+    })
+    return res.send('Ola mundo');
+});
 
 app.listen(3001);
